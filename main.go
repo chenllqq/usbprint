3808be1eef97557c06a29773f97815fbc4b71b92
chenllqq <chenllqq@163.com>
2021-03-02 01:11:06 +0000

    update new main.go

--- main.go
+++ main.go
@@ -2,33 +2,104 @@ package main
 
 import (
     "fmt"
-    "strings"
-    "github.com/tarm/goserial"
+//    "io"
+//    "strings"
+    
+    //"os"
+    //"io/ioutil"
+    //"os/exec"
+    //"strconv"
+    "time"
 )
 
-func main() {
-    cfg := &serial.Config{Name: "/dev/ttyUSB0", Baud: 115200 , ReadTimeout:3 }
+var download bool
+
+
+func task_download(){
     
-    iorwc, err := serial.OpenPort(cfg)
+    fmt.Println(" task download is running ")
+
+    download = false
     
-    if( err != nil ){
-        fmt.Println(err)
-        return
+}
+
+func task_led(){
+    var count uint
+    var key bool
+    var err string
+
+    for true{
+        count++
+        time.Sleep( time.Millisecond )
+        
+        //运行灯
+        if count == 1000 {
+            gpio_set(17, 1)//LED 1
+        }else if count == 2000 {
+            gpio_set(17, 0)//LED 1
+            count = 0
+        }
+        if download == false{
+            //按键检测
+            key, err = gpio_read(18)
+            /*if err == "" {
+                if key == true {
+                    download = true
+                    go task_download()
+                }
+            }*/
+        }
+        
     }
+}
+
+func task_usbprint(){
     
-    defer iorwc.Close()
+    for true{
+        time.Sleep(1000*time.Millisecond)
+    }
+}
 
-    buffer := make([]byte, 8000 )
 
-    num, err := iorwc.Read(buffer)
+func main() {
+    
+    
+    dev_gpio_init()
+    dev_usbprint_init()
+    
+    go task_led()
+    go task_usbprint()
+    
+    fmt.Println("program is running...")
+    
+    for true{
+    }
+    return
 
+/*
+1. 关键字 defer 用于注册延迟调用。
+2. 这些调用直到 return 前才被执。因此，可以用来做资源清理。
+3. 多个defer语句，按先进后出的方式执行。
+4. defer语句中的变量，在defer声明时就决定了。
+*/
+    //defer iorwc.Close()
+    
+    //page_pinrt_command( iorwc )
+    //pinrt_QRCode( iorwc )
+    
+   // buffer := make([]byte, 8000 )
+    
+    //num, err := iorwc.Read(buffer)
+    
     /*if err != nil {
         fmt.Println(err)
         fmt.Println("num = ", num)
         return
     }*/
     
-    num, err = iorwc.Write([]byte("AT\r\n"))
+    
+    /*
+    num, err = iorwc.Write( []byte("AT\r\n") )
     if err != nil {
         fmt.Println(err)
         return
@@ -47,9 +118,9 @@ func main() {
             break
         }
     }
-
+    */
     //打印输出读到的信息
-    fmt.Println(tmpstr)
+    //fmt.Println(tmpstr)
 
     return
 }
